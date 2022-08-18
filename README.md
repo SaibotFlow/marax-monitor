9411d07bf439bd91a781e1d74a37f654f8a18268
SaibotFlow <mail@wolf-inet.de>
2022-08-18 23:28:47 +0200

    Initial

---
+++ README.md
@@ -0,0 +1,73 @@
+# MaraX Montior + Automatic Shot-Timer
+
+Hello everyone! This project is for anyone who owns a Lelit Mara X (V2) espresso machine. When looking for a new espresso machine, one feature I always wanted was a shot timer. Usually the machines are beautifully designed so I didn't want to ruin the look by adding a $5 kitchen timer to the machine.
+So far so good. Now you are going to say : Dude the MaraX doesnt have a Shot-Timer?! Yes, you are right but the MaraX has a serial interface!
+As a developer and tinkerer a ready to use serial interface is more interessting then a ready to use Shot-Timer ;)
+
+![Display](https://github.com/SaibotFlow/marax-monitor/blob/main/assets/DisplayInAction.png?raw=true)
+ 
+# Parts
+
+- Arduino Nano (You can use any other equivalent board)
+- Breadboard
+- Jumper-Wires
+- 0,96" SSD1306 OLED Display
+
+# Structure & Connections
+
+![Fritzing](https://github.com/SaibotFlow/marax-monitor/blob/main/assets/Schema.png?raw=true)
+
+# The Interface
+
+The MaraX has a 6-PIN Connector for the serial interface on the bottom. We only use **PIN-3** (Mara RX - Black Wire - Green Rectangle) and **PIN-4** (Mara TX - Orange Wire - White Rectangle). For testing you can use the jumper-wires later you should replace them with temperature protected cables. 
+ - **PIN3 Mara** to  **D6**
+ - **PIN4 Mara** to **D5**
+
+RX Mara to TX Arudino
+TX Mara to RX Arduino
+You will receive the following data each ~400ms.
+
+![enter image description here](https://github.com/SaibotFlow/marax-monitor/blob/main/assets/Bottom.png?raw=true)
+
+The serial interface itself
+
+![enter image description here](https://github.com/SaibotFlow/marax-monitor/blob/main/assets/Connector.png?raw=true)
+  
+  ***Note**: I also did some tests with a reed sensor and the vibration pump. However its not necessary for the MaraX the interface provides the information about the status of the pump. Maybe I will do another project for a Shot-Timer only for espresso machines with a vibration pump only :-)*
+
+## The Data
+
+Example Data: **C1.06,116,124,093,0840,1,0\n**
+- The delimitter is: **,**
+- The end of data is: **\n**
+- Length: **26**
+
+|Data|Description |
+|--|--|
+| C | Machine-Mode: C = CoffeeMode; V = Vapour/SteamMode |
+| 1.06 | Firmware |
+| 116 | Current Steam Temperature in Celsius |
+| 124| Target Steam Temperature in Celsius |
+| 093| Curent Hx Temperature in Celsius |
+| 0840| Countdown Boost-Mode |
+| 1| Heat state (0 = off; 1= on) |
+| 0| Pump state (0 = off; 1= on) |
+
+
+# Display Wiring
+|Pin Display|Pin Arduino Nano|
+|--|--|
+| VIN | 3,3V or 5V |
+| GND| GND |
+| SCL| A5 |
+| SDA| A4 |
+
+The reset pin is 0x3D or 0x3C. You can recognize it by the solder joint on the back side
+
+![BackSideDisplay](https://github.com/SaibotFlow/marax-monitor/blob/main/assets/Backside.png?raw=true)
+
+## Libaries
+ - Wire.h
+ - Adafruit_GFX.h
+ - Adafruit_SSD1306.h
+ - SoftwareSerial.h
---
+++ marax_monitor/marax_monitor.ino
@@ -0,0 +1,163 @@
+//Includes
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <SoftwareSerial.h>
+
+//Defines
+#define SCREEN_WIDTH    128     //Width in px 
+#define SCREEN_HEIGHT   64      // Height in px
+#define OLED_RESET      -1
+#define SCREEN_ADDRESS  0x3C    // or 0x3D Check datasheet or Oled Display
+#define BUFFER_SIZE     32
+
+//Pins
+int d5 = 5; //orange PIN 4 Mara TX to Arduino RX D5
+int d6 = 6; //black  PIN 3 Mara RX to Arduino TX D6
+
+//Internals
+long lastMillis;
+int seconds = 0;
+int lastTimer = 0;
+long serialTimeout = 0;
+char buffer[BUFFER_SIZE];
+int index = 0;
+
+//Mara Data
+String maraData[7];
+String* lastMaraData;
+
+//Instances
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+SoftwareSerial mySerial(d5, d6);
+
+void setup()
+{
+  display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);
+  display.clearDisplay();
+  display.display();
+  Serial.begin(9600);
+  mySerial.begin(9600);
+  memset(buffer, 0, BUFFER_SIZE);
+  mySerial.write(0x11);
+}
+
+void getMaraData()
+{
+  /*
+    Example Data: C1.06,116,124,093,0840,1,0\n every ~400-500ms
+    Length: 26
+    [Pos] [Data] [Describtion]
+    0)      C     Coffee Mode (C) or SteamMode (V)
+    -        1.06  Software Version
+    1)      116   current steam temperature (Celsisus)
+    2)      124   target steam temperature (Celsisus)
+    3)      093   current hx temperature (Celsisus)
+    4)      0840  countdown for 'boost-mode'
+    5)      1     heating element on or off
+    6)      0     pump on or off
+  */
+
+  while (mySerial.available())
+  {
+    serialTimeout = millis();
+    char rcv = mySerial.read();
+    if (rcv != '\n')
+      buffer[index++] = rcv;
+    else {
+      index = 0;
+      Serial.println(buffer);
+      char* ptr = strtok(buffer, ",");
+      int idx = 0;
+      while (ptr != NULL)
+      {
+        maraData[idx++] = String(ptr);
+        ptr = strtok(NULL, ",");
+      }
+      lastMaraData = maraData;
+    }
+  }
+  if (millis() - serialTimeout > 6000)
+  {
+    serialTimeout = millis();
+    mySerial.write(0x11);
+  }
+}
+
+void updateView()
+{
+  display.clearDisplay();
+  display.setTextColor(WHITE);
+  //HX
+  display.setCursor(2, 2);
+  display.setTextSize(2);
+  display.print(lastMaraData[3].toInt());
+  display.setTextSize(1);
+  display.print((char)247);
+  display.setTextSize(1);
+  display.print("C");
+  //Pump
+  display.setCursor(2, 30);
+  display.print("H");
+  if (lastMaraData[5].toInt() == 0)
+    display.drawCircle(17, 33, 6, WHITE);
+  else
+    display.fillCircle(17, 33, 5, WHITE);
+  display.setCursor(30, 30);
+  //Heat
+  display.print("P");
+  if (lastMaraData[6].toInt() == 0)
+    display.drawRect(40, 28, 10, 10, WHITE);
+  else
+    display.fillRect(40, 28, 10, 10, WHITE);
+  //Steam
+  display.setCursor(2, 50);
+  display.setTextSize(2);
+  display.print(lastMaraData[1].toInt());
+  display.setTextSize(1);
+  display.print((char)247);
+  display.setTextSize(1);
+  display.print("C");
+  display.drawLine(55, 0, 55, 68, WHITE);
+  display.setCursor(65, 15);
+  display.setTextSize(5);
+  //Timer
+  if (seconds > 5)
+    display.print(seconds > 9 ? seconds : "0" + String(seconds));
+  else
+    display.print("00");
+  //Mode
+  display.setTextSize(1);
+  display.setCursor(120, 2);
+  display.print(lastMaraData[0].substring(0, 1));
+  //LastTimer
+  if (lastTimer > 0)
+  {
+    display.setCursor(115, 57);
+    display.print(lastTimer);
+  }
+  display.display();
+}
+
+void loop()
+{
+  getMaraData();
+  int pumpState = lastMaraData[6].toInt();
+  if (pumpState == 1)
+  {
+    if ( millis() - lastMillis >= 1000)
+    {
+      lastMillis = millis();
+      ++seconds;
+      if (seconds > 99)
+        seconds = 0;
+    }
+  }
+  else
+  {
+    if (seconds != 0)
+      lastTimer = seconds;
+    seconds = 0;
+  }
+  updateView();
+}
